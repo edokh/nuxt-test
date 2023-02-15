56eea23214efe465df0f3d497b292adff8e0bbf3
EidoKh <eido.khudyda@gmail.com>
2023-02-15 16:56:07 +0300

    first commit

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>Hello from Nuxt</h1>
+    <div>{{ temp }}</div>
+  </div>
+</template>
+<script setup>
+const { data: temp } = useFetch("https://httpbin.org/get");
+</script>
